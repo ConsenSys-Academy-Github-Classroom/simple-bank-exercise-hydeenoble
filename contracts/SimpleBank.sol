d78d5c24dddf7beae44ef440f05599405e3b37a4
Idowu Emehinola <hydeenoble39@gmail.com>
2021-10-10 16:00:16 +0200

    implemented the  and  test cases

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -19,7 +19,7 @@ contract SimpleBank {
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
@@ -29,7 +29,7 @@ contract SimpleBank {
      */
     
     // Add an argument for this event, an accountAddress
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
     event LogDepositMade();
@@ -64,6 +64,9 @@ contract SimpleBank {
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
       enrolled[msg.sender] = true;
+
+      emit LogEnrolled(msg.sender);
+
       return enrolled[msg.sender];
     }
 
